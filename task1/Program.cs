5e1fbf1e825d6765de8d858193e53dcd15967725
Dinya Suhanov <enkont@mail.ru>
2023-12-26 17:03:58 +0500

    leson3;task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,9 +3,10 @@
 int value = 100;
 Console.WriteLine(value);
 
-double d = 5,55;
+double d = 5.55;
 char c = '!';
 bool b = true;
+
 Console.WriteLine(d);
 Console.WriteLine(c);
 Console.WriteLine(b);
